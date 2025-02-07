6d17ff7c7058a3d0a5375f9a89e3aba9980e7652
KinuGra <techgit1024@gmail.com>
2025-02-07 17:23:58 +0900

    問題をランダム表示、タイトル画面

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,13 @@
 <script>
     import AnswerButton from "./AnswerButton.svelte";
     import "./app.css";
+    import {getQuizdata} from "./quizdataFactory";
+    import TitlePage from "./TitlePage.svelte";
   
+    let showTitle = true;
     let currentScore = 0;
     let renzokuSeikai = 0;
-
-    let mondai = "鮪";
-    let seikai = "まぐろ";
-    let taku = ["ひらめ", "かれい", "まぐろ", "ぶり", "くじら"];
+    let quizdata = getQuizdata();
 
     function answerButtonClicked(isCorrect){
       if(isCorrect){
@@ -17,25 +17,28 @@
       else{
         renzokuSeikai = 0;
       }
+      quizdata = getQuizdata(); // 問題を差し替える
     }
 </script>
 
-<main class = "flex flex-col h-svh">
-  <!-- メニューバー -->
-  <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
-    <div>スコア:{currentScore}</div>
-    <div>連続正解数:{renzokuSeikai}</div>
-  </div>
+{#if showTitle}
+  <TitlePage on:click={()=>showTitle=false}></TitlePage>
+{:else}
+  <main class = "flex flex-col h-svh">
+    <!-- メニューバー -->
+    <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+      <div>スコア:{currentScore}</div>
+      <div>連続正解数:{renzokuSeikai}</div>
+    </div>
 
-  <!-- 問題 -->
-  <div class="bg-green-200 text-center text-4xl py-4">{mondai}</div>
+    <!-- 問題 -->
+    <div class="bg-green-200 text-center text-4xl py-4">{quizdata.mondai}</div>
 
-  <!-- 選択肢 -->
-  <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
-    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[0])}>{taku[0]}</AnswerButton>
-    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[1])}>{taku[1]}</AnswerButton>
-    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[2])}>{taku[2]}</AnswerButton>
-    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[3])}>{taku[3]}</AnswerButton>
-    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[4])}>{taku[4]}</AnswerButton>
-  </div>
-</main>
+    <!-- 選択肢 -->
+    <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+      {#each quizdata.taku as t}
+        <AnswerButton on:click={() => answerButtonClicked(quizdata.seikai===t)}>{t}</AnswerButton>
+      {/each}
+    </div>
+  </main>
+{/if}
