132f72d906d3a38a8ae360b171023e63efb2474c
KinuGra <techgit1024@gmail.com>
2025-02-07 15:34:56 +0900

    外観、ボタンの作成 正解クリック時のイベントを作成 問題、選択肢の変数化

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+    import AnswerButton from "./AnswerButton.svelte";
+    import "./app.css";
+  
+    let currentScore = 0;
+    let renzokuSeikai = 0;
+
+    let mondai = "鮪";
+    let seikai = "まぐろ";
+    let taku = ["ひらめ", "かれい", "まぐろ", "ぶり", "くじら"];
+
+    function answerButtonClicked(isCorrect){
+      if(isCorrect){
+        renzokuSeikai += 1;
+        currentScore += renzokuSeikai;
+      }
+      else{
+        renzokuSeikai = 0;
+      }
+    }
+</script>
+
+<main class = "flex flex-col h-svh">
+  <!-- メニューバー -->
+  <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+    <div>スコア:{currentScore}</div>
+    <div>連続正解数:{renzokuSeikai}</div>
+  </div>
+
+  <!-- 問題 -->
+  <div class="bg-green-200 text-center text-4xl py-4">{mondai}</div>
+
+  <!-- 選択肢 -->
+  <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[0])}>{taku[0]}</AnswerButton>
+    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[1])}>{taku[1]}</AnswerButton>
+    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[2])}>{taku[2]}</AnswerButton>
+    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[3])}>{taku[3]}</AnswerButton>
+    <AnswerButton on:click={() => answerButtonClicked(seikai===taku[4])}>{taku[4]}</AnswerButton>
+  </div>
+</main>
